c5a5e469b022fd80971bfa9020a977a3dc3c25f3
aamartinez25 <54689493+aamartinez25@users.noreply.github.com>
2019-08-29 15:09:49 -0700

    Add files via upload

---
+++ kingdom.py
@@ -0,0 +1,23 @@
+#
+#Author: Adrian Martinez
+#Description: Program that prints out five lines from the movie Kingdom of Heaven, chess scene
+#
+
+print('----- KINGDOM OF HEAVEN -----')
+print() #blank spaces
+print()
+print('KING: Do you play?')
+print()
+print('BALIAN: No.')
+print()
+print('KING: The whole world is in chess. Any move can be the death of you. Do anything')
+print('      except remain where you started and you can\'t be sure of your end. Were')
+print('      you sure of your end once?')
+print()
+print('BALIAN: I was.')
+print()
+print('KING: What was it?')
+print()
+print('BALIAN: To be buried 100 yards from where I was born.')
+print()
+print('KING: And now?')
---
+++ rogueone.py
@@ -0,0 +1,19 @@
+#
+#Author: Adrian Martinez
+#Description: Program that prints out five lines from the movie Rogue One: A Star Wars Story
+#
+
+print('----- ROGUE ONE -----')
+print() #blank spaces
+print()
+print('BODHI: That\'s him. That\'s him, Galen. In the dark suit.')
+print()
+print('CASSIAN: Get back down there, and find us a ride out of here.')
+print()
+print('BODHI: What will you do?')
+print()
+print('CASSIAN: You heard me.')
+print()
+print('BODHI: You said we came up here just to have a look.')
+print()
+print('CASSIAN: I\'m here. I\'m looking. Go. Hurry!')
