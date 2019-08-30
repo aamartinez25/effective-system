daac27d50e33b900081468b821fa6368cd2fb9ac
aamartinez25 <54689493+aamartinez25@users.noreply.github.com>
2019-08-30 17:37:44 -0600

    Update tie.py

--- tie.py
+++ tie.py
@@ -3,7 +3,7 @@
 #Description: Program that creates a tie fighter with user input width
 #
 
-width = int(input('Enter TIE width: /n')) #initial width input from user
+width = int(input('Enter TIE width: \n')) #initial width input from user
 center_width = '         ' #var for center tie width
 
 print('|[' + ' ' * width + center_width + ' ' * width +']|')
