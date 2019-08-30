8cfef2eee926f28aec0a926e8a78df7eb3931325
aamartinez25 <54689493+aamartinez25@users.noreply.github.com>
2019-08-30 16:20:01 -0700

    Add files via upload

---
+++ tie.py
@@ -0,0 +1,12 @@
+width = int(input('Enter TIE width: '))
+center_width = '         '
+tie_width = ' ' * width
+print('|[' + ' ' * width + center_width + ' ' * width +']|')
+print('|[' + ' ' * width + center_width + ' ' * width +']|')
+print('|[' + ' ' * width + ' /=---=\\ ' + ' ' * width + ']|')
+print('|[' + ' ' * width + '/==---==\\' + ' ' * width + ']|')
+print('|[' + '/' * width + '|== X ==|' + '\\' * width + ']|')
+print('|[' + ' ' * width + '\\==---==/' + ' ' * width + ']|')
+print('|[' + ' ' * width + ' \\=---=/ ' + ' ' * width + ']|')
+print('|[' + ' ' * width + center_width + ' ' * width +']|')
+print('|[' + ' ' * width + center_width + ' ' * width +']|')
