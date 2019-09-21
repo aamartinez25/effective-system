b2ac4de846326e7dcce04cdfd0639f7e00bd2e70
aamartinez25 <54689493+aamartinez25@users.noreply.github.com>
2019-09-21 16:19:18 -0700

    Update cab.py

--- cab.py
+++ cab.py
@@ -5,17 +5,17 @@
 # through remaining digit outputs. First two inputs are added together to create
 # a line.
 #
-bars = input('Enter bar string:\n')     #user bar string input
+bars = input('Enter bar string:\n')                 #user bar string input
 
 index = 0
-bar_height = len(bars) -1               #Variable used to determine how many bars will be printed
+bar_height = len(bars) -1                           #Variable used to determine how many bars will be printed
 
 print('+------------------+')
-while index <= bar_height:              #While loop used to print out hashtag bars
-    bar_length_left = '#' * int(bars[index])
-    bar_length_right = '#' * int(bars[index + 1])                        #Variable for printed bar hashtags from user inputs
-    space_left = ' ' * int((9 - int(bars[index]) / 2))
-    space_right = ' ' * int((9 - int(bars[index + 1]) / 2)) #Variable for spacing
+while index <= bar_height:                          #While loop used to print out hashtag bars
+    bar_length_left = '#' * int(bars[index])        #Variable for printed bar hashtags from user inputs
+    bar_length_right = '#' * int(bars[index + 1])   #Variable for using next input
+    space_left = ' ' * (9 - int(bars[index]))       #Variable for spacing
+    space_right = ' ' * (9 - int(bars[index + 1]))  #Variable for using next string as base for right side spacing
     print('|' + space_left +  bar_length_left + bar_length_right + space_right + '|\n', end = '')
     index += 2
 print('+------------------+')
